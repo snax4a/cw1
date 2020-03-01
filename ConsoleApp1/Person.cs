09fa4d4c00511e71027ecdd31e0017af5ad7f710
Szymon Sus <s16785@pjwstk.edu.pl>
2020-03-01 14:17:02 +0100

    Created simple http email crawler

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw1
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl/";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex(@"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            } 
+        }
+    }
+}
